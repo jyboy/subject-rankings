51044ea0b306aa2daed90a89c96f3e2e015c8c73
jyboy <hitwangjunyang@163.com>
2018-08-19 02:29:13 +0800

    fix cdn

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 const HOST_URL = 'https://api.atsjtu.cc/subjectRanking';
-const CDN_URL = 'http://p1xf71d56.bkt.clouddn.com';
+const CDN_URL = 'https://cdn.atsjtu.cc/subject-ranking';
 
 module.exports = {
-    REMARKS_URL: HOST_URL + '/getRemarks',
-    POSTER_URL: CDN_URL + '/images/poster_subjectRanking.png',
-    getWikiUrl: (link) => CDN_URL + '/images/' + link,
-    getUniversityRankingUrl: (subjectCode, first) => HOST_URL + '/universityRanking?subjectCode=' + subjectCode + '&first=' + first,
-    getSubjectRankingUrl: universityNum => HOST_URL + '/subjectRanking?universityNum=' + universityNum
+    REMARKS_URL: `${HOST_URL}/getRemarks`,
+    POSTER_URL: `${CDN_URL}/poster_subjectRanking.png`,
+    getWikiUrl: (link) => `${CDN_URL}/${link}`,
+    getUniversityRankingUrl: (subjectCode, first) => `${HOST_URL}/universityRanking?subjectCode=${subjectCode}&first=${first}`,
+    getSubjectRankingUrl: universityNum => `${HOST_URL}/subjectRanking?universityNum=${universityNum}`
 };
